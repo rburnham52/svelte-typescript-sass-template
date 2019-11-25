c2b366e5b58fdd862a364eeabcf90258fe313439
Ryan Burnham <Ryan.Burnham@Definitiv.com.au>
2019-11-25 09:58:56 +0800

    Updated svelte.config.js to match webpack setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,29 +1,13 @@
-const sass = require('node-sass');
-// This adds Language Server Protocol support for tools like vscode
-// related: https://github.com/UnwrittenFun/svelte-vscode/issues/1
+const {aliases, scssAliases} = require("./webpack.parts");
+
+const sveltePreprocess = require('svelte-preprocess');
 
 module.exports = {
-    preprocess: {
-        style: async ({ content, attributes }) => {
-            if (!['text/sass', 'text/scss'].some(attributes.type) && !['sass', 'scss'].some(attributes.lang)) return;
-
-            return new Promise((resolve, reject) => {
-                sass.render(
-                    {
-                        data: content,
-                        sourceMap: true,
-                        outFile: 'x', // this is necessary, but is ignored
-                    },
-                    (err, result) => {
-                        if (err) return reject(err);
-
-                        resolve({
-                            code: result.css.toString(),
-                            map: result.map.toString(),
-                        });
-                    },
-                );
-            });
-        },
-    },
+    preprocess: sveltePreprocess({
+        scss: {
+            importer: [
+                scssAliases(aliases),
+            ],
+        }
+    }),
 };
